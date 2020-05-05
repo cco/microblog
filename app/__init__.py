21a65b5fa817aa13036649dee1cee56ddb7d29da
cco <14793691+cco@users.noreply.github.com>
2020-05-05 16:06:36 -0700

    Created structure for login handling python module and login template

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__) # var app created as an instance of Flask. This makes var app a member of app package (directory is package).
+app.config.from_object(Config) # Tells Flask to read and apply configuration from Config class within config.py
 
-from app import routes
+from app import routes # ref to app here is referring to directory app. Importing routes.py
