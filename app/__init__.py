354bb54ac9a6edcf7415e52ab9f3a15b7e9cef11
cco <14793691+cco@users.noreply.github.com>
2020-05-05 18:50:33 -0700

    Created starting structure to database modelling, implemented database migration with Alembic

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__) # var app created as an instance of Flask. This makes var app a member of app package (directory is package).
 app.config.from_object(Config) # Tells Flask to read and apply configuration from Config class within config.py
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes # ref to app here is referring to directory app. Importing routes.py
+from app import routes, models # ref to app here is referring to directory app. Importing routes.py
