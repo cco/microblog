f50b78441d5038f964741d24a91c246b1e8963bd
cco <14793691+cco@users.noreply.github.com>
2020-05-05 17:22:16 -0700

    Implemented basic field validation and redirect on invalid data.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # Looks for env var or uses hardcoded string.
