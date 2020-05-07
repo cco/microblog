ca84af5e0ad5f6b3f94b8ade0e5153dace4fa8cb
cco <14793691+cco@users.noreply.github.com>
2020-05-07 15:27:16 -0700

    Added master branch

--- app/routes.py
+++ app/routes.py
@@ -57,3 +57,13 @@ def register():
         flash('Congrats, you are now registered!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Testp post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
