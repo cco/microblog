ace939d9989ca56b940d6d9c4527ea6cf9d64e56
cco <14793691+cco@users.noreply.github.com>
2020-05-08 15:08:50 -0700

    Implemented db error handling on same usernames. Implemented basic email notifications. Implemented rotating application logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+import os
+import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__) # var app created as an instance of Flask. This makes var app a member of app package (directory is package).
 app.config.from_object(Config) # Tells Flask to read and apply configuration from Config class within config.py
@@ -12,3 +15,24 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors # ref to app here is referring to directory app. Importing routes.py
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))                                   
