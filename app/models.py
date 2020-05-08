41235ab008279a4cffd04d45ca65a3a783feabc2
cco <14793691+cco@users.noreply.github.com>
2020-05-07 17:31:00 -0700

    Added about me and username options

--- app/models.py
+++ app/models.py
@@ -10,6 +10,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
